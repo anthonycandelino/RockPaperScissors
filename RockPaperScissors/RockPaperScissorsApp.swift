f0b05733d59cdfe27b93cf6e6d87566aed824e13
Anthony Candelino <anthcandelino@gmail.com>
2024-06-14 13:11:00 -0700

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Anthony Candelino on 2024-06-14.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
